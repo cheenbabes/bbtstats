253645d73e009c5695319e2ab4ff141c09d061da
Eugene Baibourine <eugene.baibourin@gmail.com>
2017-04-07 18:00:37 -0600

    Scope is working properly and dashboard is setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,22 @@
-app.controller('MainCtrl', [function() {
+app.controller('MainCtrl', function($scope) {
 
-}]);
+    var d = new Date();
+    var n = d.getMonth();
+ 
+    $scope.months = [
+        { id: 'Jan', label:'January'},
+        { id: 'Feb', label:'February'},
+        { id: 'Mar', label:'March'},
+        { id: 'Apr', label:'April'},
+        { id: 'May', label:'May'},
+        { id: 'Jun', label:'June'},
+        { id: 'Jul', label:'July'},
+        { id: 'Aug', label:'August'},
+        { id: 'Sep', label:'September'},
+        { id: 'Oct', label:'October'},
+        { id: 'Nov', label:'November'},
+        { id: 'Dec', label:'December'}
+    ];
+    
+    $scope.currMonth = $scope.months[n];
+});
